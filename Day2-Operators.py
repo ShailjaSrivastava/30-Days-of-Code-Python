59bbdd7e089cd6c058e2c8574faf3f8e05596e82
Shailja Srivastava <shailjavastav89@gmail.com>
2021-05-17 11:19:17 +0530

    Added the code

--- Day2-Operators.py
+++ Day2-Operators.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-
 import math
 import os
 import random
@@ -21,11 +20,10 @@ def solve(meal_cost, tip_percent, tax_percent):
     tip_percent = (meal_cost/100)*tip_percent
     meal_cost=meal_cost+tip_percent+tax_percent
     print(round(meal_cost))
+    
 if __name__ == '__main__':
     meal_cost = float(input().strip())
-
     tip_percent = int(input().strip())
-
     tax_percent = int(input().strip())
 
     solve(meal_cost, tip_percent, tax_percent)
